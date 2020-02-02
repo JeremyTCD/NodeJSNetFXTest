8606db30c72d5c220d1d59d093e8869a0469905e
JeremyTCD <jeremytanchongdao@gmail.com>
2020-02-02 13:31:10 +0800

    Initial

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using Jering.Javascript.NodeJS;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace NodeJSNetFXTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            IServiceCollection services = new ServiceCollection();
+            services.AddNodeJS();
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            INodeJSService nodeJSService = serviceProvider.GetRequiredService<INodeJSService>();
+
+            int result = nodeJSService.InvokeFromStringAsync<int>("module.exports = (callback) => callback(null, 1)").GetAwaiter().GetResult();
+
+            Console.WriteLine(result);
+        }
+    }
+}
